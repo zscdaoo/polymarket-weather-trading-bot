d52f3df9e9e7dafc26ba4d711968325d2603d8bb
olzhang <oliver.zhang2718@gmail.com>
2025-02-16 11:11:24 -0300

    update 1.6

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", this.window.scrollY > 0)
 });
+
+let menu = document.querySelector('#menu-iocn');
+let navigation = document.querySelector('.navigation');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navigation.classList.toggle('open');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navigation.classList.remove('open');
+}
